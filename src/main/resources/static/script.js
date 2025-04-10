eaaf7fc1d437dcec0be687f37181f9cd128add97
oscarlopeeez <202215073@alu.comillas.edu>
2025-04-10 12:06:37 +0200

    j

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -9,6 +9,17 @@ const verTablaUno = (tel_peluquero) => {
 const verTablaTodos = () => {
     console.log("enseñando tabla de todos")
 }
+document.getElementById("cita").addEventListener("submit", function(event){
+    event.preventDefault(); // Evitar el envío del formulario
+
+    const clienteID = document.getElementById("clienteID").value;
+    const peluqueroID = document.getElementById("peluqueroID").value;
+    const fecha = document.getElementById("fecha").value;
+    const hora = document.getElementById("hora").value;
+
+    registrarCita(clienteID, peluqueroID, fecha, hora);
+})
+
 function registrarCita(clienteID, peluqueroID, fecha, hora) {
     const data = {
         clienteID: clienteID,
