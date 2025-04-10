5c714e4a763fef570b8cd9a2e6d9dc2376d22ca9
oscarlopeeez <202215073@alu.comillas.edu>
2025-04-10 12:00:09 +0200

    f

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -9,3 +9,26 @@ const verTablaUno = (tel_peluquero) => {
 const verTablaTodos = () => {
     console.log("enseñando tabla de todos")
 }
+function registrarCita(clienteID, peluqueroID, fecha, hora) {
+    const data = {
+        clienteID: clienteID,
+        peluqueroID: peluqueroID,
+        fecha: fecha,
+        hora: hora
+    };
+
+    fetch('/registrarCita', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Cita registrada:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
