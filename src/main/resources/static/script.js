2944b29c1c1e1e6888ec00a7560ffb955436e074
EvaChicoErcilla <202209197@alu.comillas.edu>
2025-04-10 11:40:15 +0200

    Eva1

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,11 @@
+//funciones de actualizar tablas
+//cliente no VIP
+
+//cliente VIP --> quiero ver la disponibilidad del peluquero
+const verTablaUno = (tel_peluquero) => {
+    console.log(tel_peluquero);
+}
+
+const verTablaTodos = () => {
+    console.log("enseñando tabla de todos")
+}
